a71ae43aa6019ec4ddb71d610faee2c5778fa71b
majorz <zahari@resin.io>
2018-04-11 18:04:00 +0300

    wpa_supplicant logging using pydbus

--- ssids.py
+++ ssids.py
@@ -7,22 +7,25 @@ import gi
 gi.require_version('NM', '1.0')
 from gi.repository import NM
 
+from pydbus import SystemBus
+
+WPAS_BUS_NAME = 'fi.w1.wpa_supplicant1'
+
+def set_wpa_log_level(level):
+    bus = SystemBus()
+    ws = bus.get('fi.w1.wpa_supplicant1', '/fi/w1/wpa_supplicant1')
+
+    if level == "msgdump":
+        ws.DebugTimestamp = True
+
+    ws.DebugLevel = level
+
+    print("wpa_supplicant log level set to", level)
+
 def set_nm_log_level(nmc, level):
     nmc.set_logging(level, "all")
 
     print("NetworkManager log level set to", level)    
-
-def set_wpa_log_level(level):
-    if level == "msgdump":
-        os.system('dbus-send --system --dest=fi.w1.wpa_supplicant1 ' \
-            '/fi/w1/wpa_supplicant1 org.freedesktop.DBus.Properties.Set ' \
-            'string:fi.w1.wpa_supplicant1 string:DebugTimestamp variant:boolean:true')
-
-    os.system('dbus-send --system --dest=fi.w1.wpa_supplicant1 ' \
-        '/fi/w1/wpa_supplicant1 org.freedesktop.DBus.Properties.Set ' \
-        'string:fi.w1.wpa_supplicant1 string:DebugLevel variant:string:"{}"'.format(level))
-
-    print("wpa_supplicant log level set to", level)
 
 def get_interface():
     if len(sys.argv) < 2:
@@ -35,10 +38,11 @@ def get_interface():
 
 def get_device(nmc, interface):
     device = nmc.get_device_by_iface(interface)
-    print("Device path:", device)
 
     if device is None:
         sys.exit("Interface not found: {}".format(interface))
+
+    print_device_info(device)
 
     return device
 
@@ -54,7 +58,30 @@ def request_scan(device):
 
     print("WiFi scan requested")
 
+def ssid_to_utf8(ap):
+    ssid = ap.get_ssid()
+    if not ssid:
+        return ""
+    return NM.utils_ssid_to_utf8(ap.get_ssid().get_data())
+
+def print_device_info(device):
+    active_ap = device.get_active_access_point()
+    ssid = None
+    if active_ap is not None:
+        ssid = ssid_to_utf8(active_ap)
+    info = "Device: %s | Driver: %s | Active AP: %s" % (device.get_iface(), device.get_driver(), ssid)
+    print(info)
+
+def print_ap_info(device):
+    active_ap = device.get_active_access_point()
+    ssid = None
+    if active_ap is not None:
+        ssid = ssid_to_utf8(active_ap)
+    info = "Active AP: %s" % (ssid, )
+    print(info)
+
 def main():
+
     interface = get_interface()
 
     nmc = NM.Client.new(None)
@@ -67,17 +94,18 @@ def main():
     device = get_device(nmc, interface)
 
     while True:
-        print("Sleeping 5 seconds...")
+        print("Sleeping 10 seconds...")
 
         try:
-            time.sleep(5)
+            time.sleep(10)
         except KeyboardInterrupt:
             break
 
-        count = get_access_point_count(device)
-        count = 0
+        print_ap_info(device)
 
-        if count == 0:
+        count = get_access_point_count(device)
+
+        if count == 1:
             print("No access points available")
 
             zero_times += 1
@@ -91,9 +119,8 @@ def main():
             time.sleep(10)
 
             count = get_access_point_count(device)
-            count = 0
 
-            if count == 0:
+            if count == 1:
                 print("Scanning did NOT work")
             else:
                 print("Scanning WORKED!!!")
